234535717419813461c764c9aee9a74a0f2347f3
willpxxr <williamparr96@outlook.com>
2022-01-11 17:18:35 +0000

    feat: Add feature flags, finalize home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,19 @@ import {NavbarMain,} from "./components/NavbarMain";
 import {Container,} from "react-bootstrap";
 import Contributions from "./components/Contributions";
 import {Home} from "./components/Home";
-
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css'
+import {Feature, FeatureFlag, FeatureFlags, isFeatureEnabled} from "./components/FeatureFlags";
 
 interface DecoratedRouteObject extends RouteObject {
     display?: String,
     children?: DecoratedRouteObject[]
+}
+
+const RouteOrNull = (featureFlag: String, route: DecoratedRouteObject) => {
+    if (isFeatureEnabled(featureFlag)) {
+        return route
+    } else return null
 }
 
 const routes: DecoratedRouteObject[] = [
@@ -19,39 +25,54 @@ const routes: DecoratedRouteObject[] = [
         element: <Home/>,
         display: "Home",
     },
-    {
+    RouteOrNull("contributions", {
         path: "/contributions",
-        element: <Contributions/>,
+        element: <Feature name={"contributions"}><Contributions/></Feature>,
         display: "Contributions",
-    },
-    {
+    }),
+    RouteOrNull("about", {
         path: "/about",
-        element: <B/>,
+        element: <Feature name={"about"}><About/></Feature>,
         display: "About",
-    },
+    }),
     {
         path: "*",
         element: <Navigate to="/"/>
     }
+].filter((route) => (route != null)) as DecoratedRouteObject[]
+
+const flags: FeatureFlag[] = [
+    {
+        name: "contributions",
+        description: "Whether contributions area should be enabled or not",
+        enabled: false,
+    },
+    {
+        name: "about",
+        description: "Whether about area should be enabled or not",
+        enabled: false,
+    },
 ]
 
 export default function App() {
     return (
-        <div className="min-vh-100 d-flex flex-column bg-gunmetal">
-            <NavbarMain routes={
-                routes.filter((route) => route.display != null).map((route => {
-                    return {
-                        path: route.path as String,
-                        display: route.display!!,
-                    }
-                }))
-            }/>
-            { useRoutes(routes) }
-        </div>
+        <FeatureFlags flags={flags}>
+            <div className="min-vh-100 d-flex flex-column bg-gunmetal">
+                <NavbarMain routes={
+                    routes.filter((route) => route.display != null).map((route => {
+                        return {
+                            path: route.path as String,
+                            display: route.display!!,
+                        }
+                    }))
+                }/>
+                { useRoutes(routes) }
+            </div>
+        </FeatureFlags>
     );
 }
 
-function B() {
+function About() {
     return (
         <Container fluid>
             <h1>B</h1>
---
+++ frontend/src/components/FeatureFlags.tsx
@@ -0,0 +1,47 @@
+import React, {FC} from "react";
+
+export interface FeatureFlag {
+    name: string,
+    description: string,
+    enabled: boolean,
+}
+
+export interface FeatureFlagsProps {
+    flags: FeatureFlag[]
+}
+
+const flagsKey = "flags"
+
+export const FeatureFlags: FC<FeatureFlagsProps> = (props) => {
+    if (!localStorage.getItem(flagsKey)) {
+        localStorage.setItem(flagsKey, JSON.stringify(props.flags))
+    }
+    return (props.children as React.ReactElement)
+}
+
+export interface FeatureProps {
+    name: string
+}
+
+export const Feature: FC<FeatureProps> = (props) => {
+    if (isFeatureEnabled(props.name)) {
+        return (props.children as React.ReactElement)
+    }
+
+    return null
+}
+
+export const isFeatureEnabled = (featureName: String) => {
+    const features = localStorage.getItem(flagsKey)
+    if (features) {
+        const feature = (JSON.parse(features) as FeatureFlag[])?.find((feature) => (
+            feature.name === featureName
+        ))
+
+        if (typeof feature != "undefined" && feature.enabled) {
+            return true
+        }
+    }
+
+    return false
+}
